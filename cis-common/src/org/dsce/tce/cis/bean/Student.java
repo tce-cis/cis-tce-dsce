9d2c2183d668eb45fa51963f541e10111a9a8890
Chetan <chetan.dev.adm@gmail.com>
2017-05-03 20:49:52 +0530

    Sample bean for reference

--- cis-common/src/org/dsce/tce/cis/bean/Student.java
+++ cis-common/src/org/dsce/tce/cis/bean/Student.java
@@ -2,4 +2,34 @@ package org.dsce.tce.cis.bean;
 
 public class Student {
 
+	// TODO: Namana to add other required attributes and corresponding getter,
+	// setter methods.
+	private String fullName;
+	private long eid;
+	private String usn;
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public long getEid() {
+		return eid;
+	}
+
+	public void setEid(long eid) {
+		this.eid = eid;
+	}
+
+	public String getUsn() {
+		return usn;
+	}
+
+	public void setUsn(String usn) {
+		this.usn = usn;
+	}
+
 }
