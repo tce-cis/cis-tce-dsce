b0f16527dd7e25e5ec4ebeaabad61d341e4b6103
Chetan <chetan.dev.adm@gmail.com>
2017-05-11 22:15:34 +0430

    Code to read publication data into DB

--- cis-util/src/org/dsce/tce/cis/util/ExcelSheetReader.java
+++ cis-util/src/org/dsce/tce/cis/util/ExcelSheetReader.java
@@ -13,18 +13,25 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.dsce.tce.cis.bean.Faculty;
+import org.dsce.tce.cis.bean.Publication;
 
 import com.google.gson.Gson;
 
-public class ExcelToJsonConvetor {
+public class ExcelSheetReader {
 
 	public static void main(String[] args) throws IOException {
-		ExcelToJsonConvetor excelReader = new ExcelToJsonConvetor();
+		ExcelSheetReader excelReader = new ExcelSheetReader();
 		String excelFilePath = "data/faculty_details.xlsx";
 		FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
-
 		Workbook workbook = new XSSFWorkbook(inputStream);
 		excelReader.readFacultyDetailsSheet(workbook.getSheetAt(0));
+		workbook.close();
+		inputStream.close();
+
+		excelFilePath = "data/publication.xlsx";
+		inputStream = new FileInputStream(new File(excelFilePath));
+		workbook = new XSSFWorkbook(inputStream);
+		excelReader.readPublicationSheet(workbook.getSheetAt(0));
 		workbook.close();
 		inputStream.close();
 
@@ -78,14 +85,56 @@ public class ExcelToJsonConvetor {
 				cellCount++;
 			}
 			facultyList.add(faculty);
-			for (Faculty member : facultyList) {
-				System.out.println(member.getEmailId());
-			}
-			System.out.println();
 		}
 		System.out.println(new Gson().toJson(facultyList));
+		JDBCUtil.persistFacultyData(facultyList);
+	}
 
-		// Caution(Chetan): Never run below method again
-		// JDBCUtil.persistFacultyData(facultyList);
+	private void readPublicationSheet(Sheet publicationsSheet) {
+		Iterator<Row> iterator = publicationsSheet.iterator();
+		List<Publication> publicationList = new ArrayList<>();
+		int cellCount;
+		Publication publication = new Publication();
+		while (iterator.hasNext()) {
+
+			cellCount = 1;
+
+			publication = new Publication();
+			Row nextRow = iterator.next();
+			Iterator<Cell> cellIterator = nextRow.cellIterator();
+
+			while (cellIterator.hasNext()) {
+
+				Cell cell = cellIterator.next();
+				cell.setCellType(Cell.CELL_TYPE_STRING);
+				switch (cell.getCellType()) {
+				case Cell.CELL_TYPE_STRING:
+					switch (cellCount) {
+					case 1:
+						// publication.setTitle(cell.getStringCellValue().trim());
+						break;
+					case 2:
+						publication.setTitle(cell.getStringCellValue().trim());
+						break;
+					case 3:
+						publication.setJournal(cell.getStringCellValue().trim());
+						break;
+					case 4:
+						publication.setPrimaryAuthor(cell.getStringCellValue().trim());
+						break;
+					case 5:
+						publication.setCoAuthors(cell.getStringCellValue().trim());
+						break;
+					}
+				default:
+					break;
+				}
+				cellCount++;
+			}
+			publicationList.add(publication);
+			System.out.println();
+		}
+		System.out.println(new Gson().toJson(publicationList));
+		JDBCUtil.persistPublicationData(publicationList);
 	}
 }
--- cis-util/src/org/dsce/tce/cis/util/JDBCUtil.java
+++ cis-util/src/org/dsce/tce/cis/util/JDBCUtil.java
@@ -1,6 +1,5 @@
 package org.dsce.tce.cis.util;
 
-//STEP 1. Import required packages
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,63 +7,83 @@ import java.sql.Statement;
 import java.util.List;
 
 import org.dsce.tce.cis.bean.Faculty;
+import org.dsce.tce.cis.bean.Publication;
 
 public class JDBCUtil {
-	// JDBC driver name and database URL
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	static final String DB_URL = "jdbc:mysql://localhost:3306/cis_tce_dsce";
 
-	// Database credentials
 	static final String USER = "root";
 	static final String PASS = "";
 
 	public static void persistFacultyData(List<Faculty> facultyList) {
-		Connection conn = null;
+		Connection dbConnection = null;
 		Statement stmt = null;
 		try {
-			// STEP 2: Register JDBC driver
 			Class.forName("com.mysql.jdbc.Driver");
-
-			// STEP 3: Open a connection
-			System.out.println("Connecting to a selected database...");
-			conn = DriverManager.getConnection(DB_URL, USER, PASS);
-			System.out.println("Connected database successfully...");
-
-			// STEP 4: Execute a query
-			System.out.println("Inserting records into the table...");
-			stmt = conn.createStatement();
+			dbConnection = DriverManager.getConnection(DB_URL, USER, PASS);
+			stmt = dbConnection.createStatement();
 
 			for (Faculty faculty : facultyList) {
 
-				String sql = "INSERT INTO cis_tce_dsce.faculty ( full_name, designation, education_qualification, experience, specialization, email, phone) "
+				String queryString = "INSERT INTO cis_tce_dsce.faculty ( full_name, designation, education_qualification, experience, specialization, email, phone) "
 						+ "VALUES ('" + faculty.getName() + "', '" + faculty.getDesignation() + "','"
 						+ faculty.getEducationalQualification() + "'," + Integer.parseInt(faculty.getExperienceYears())
 						+ ",'" + faculty.getSpecialization() + "','" + faculty.getEmailId() + "',"
 						+ faculty.getPhoneNumber() + "); ";
-				stmt.executeUpdate(sql);
+				System.out.println(queryString);
+				// stmt.executeUpdate(queryString);
 			}
-			System.out.println("Inserted records into the table...");
-
 		} catch (SQLException se) {
-			// Handle errors for JDBC
 			se.printStackTrace();
 		} catch (Exception e) {
-			// Handle errors for Class.forName
 			e.printStackTrace();
 		} finally {
-			// finally block used to close resources
 			try {
 				if (stmt != null)
-					conn.close();
+					dbConnection.close();
 			} catch (SQLException se) {
-			} // do nothing
+			}
 			try {
-				if (conn != null)
-					conn.close();
+				if (dbConnection != null)
+					dbConnection.close();
 			} catch (SQLException se) {
 				se.printStackTrace();
-			} // end finally try
-		} // end try
-		System.out.println("Goodbye!");
-	}// end main
-}// end JDBCExample
+			}
+		}
+	}
+
+	public static void persistPublicationData(List<Publication> publicationList) {
+		Connection dbConnection = null;
+		Statement stmt = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			dbConnection = DriverManager.getConnection(DB_URL, USER, PASS);
+			stmt = dbConnection.createStatement();
+			for (Publication publication : publicationList) {
+
+				String queryString = "INSERT INTO cis_tce_dsce.publication ( title, journal, primary_author, co_authors) "
+						+ "VALUES ('" + publication.getTitle() + "', '" + publication.getJournal() + "','"
+						+ publication.getPrimaryAuthor() + "','" + publication.getCoAuthors() + "');";
+				System.out.println(queryString);
+				// stmt.executeUpdate(queryString);
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (stmt != null)
+					dbConnection.close();
+			} catch (SQLException se) {
+			}
+			try {
+				if (dbConnection != null)
+					dbConnection.close();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			}
+		}
+	}
+}
